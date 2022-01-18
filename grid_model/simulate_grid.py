da4973e9933fe35f5eab162d45a3a0515727e192
Ruhollah Shemirani <shemirani.r@gmail.com>
2022-01-18 14:27:02 -0800

    a reusable code with OOP pr added

--- grid_model/simulate_grid.py
+++ grid_model/simulate_grid.py
@@ -2,23 +2,161 @@ import msprime,argparse
 import numpy as np
 from functools import partial
 
-
-
-def check_set_symmetric_migration_change(d1, d2, migration_rate, demography, height, width):
-    if d2[0] >= 0 and d2[0] < height:
-        if d2[1] >= 0 and d2[1] < width:
-            #print(f'd{d1[0]+1}_{d1[1]+1} - {d2[0]+1}_{d2[1]+1}')
-            demography.set_symmetric_migration_rate([name_generator(d1[0],d1[1]),name_generator(d2[0],d2[1])],migration_rate)
+class CellDemography:
+    
+    def __init__(self,height,width,migration_rate) -> None:
+        self.pop = msprime.Demography()
+        self.height = height
+        self.width = width
+        self.migration_rate = migration_rate
+        self.deme_count = height*width
+        self.ne = None
+        self.ss = None
+        self.samples = {}
+    @staticmethod
+    def name_generator(row,col):
+        return f'd{row+1}_{col+1}'
+    def migration_options(self,row,col):
+        return zip([row, row, row-1, row+1],[col-1, col+1, col, col])
+    def check_migration_path(self,d1,d2):
+        if d2[0] >= 0 and d2[0] < self.height:
+            if d2[1] >= 0 and d2[1] < self.width:
+                return True
+                #print(f'd{d1[0]+1}_{d1[1]+1} - {d2[0]+1}_{d2[1]+1}')
+                mig_function([name_generator(d1[0],d1[1]),name_generator(d2[0],d2[1])],migration_rate)
+        return False
+    def add_migration_path(self,d1,d2):
+        if self.check_migration_paths(d1,d2):
+            self.pop.set_symmetric_migration_rate([self.name_generator(d1[0],d1[1]),self.name_generator(d2[0],d2[1])],self.migration_rate)
             return True
-    return False
+        return False
+    def setup_migration(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                d1 = CellDemography.name_generator(i,j)
+                migration_options = lambda x: CellDemography.name_generator(*x), self.migration_options(i,j)
+                res = list(map(lambda d2: self.add_migration_path(d1,d2),migration_options))
+    def set_size(self,val,key):
+        if len(val) > 1:
+            if len(val) != self.deme_count:
+                raise ValueError(f'Mismatch between the number of sizes passed and number of available demes for {key}!')
+            self.__dict__[key] = np.array(val).reshape((self.height,self.width))
+            self.__dict__['multi_'+val] = True
+        elif len(val) == 1 :
+            self.__dict__[key] = val[0]
+            self.__dict__['multi_'+val] = False
+    def set_effective_size(self,ne):
+        self.set_size(ne,'ne')
+      
+    def set_sample_size(self,ss):
+        self.set_size(ss,'ss')
+    def get_size(self,row,col,key):
+        if self.__dict__['multi_'+key]:
+            return self.__dict__[key][row,col]
+        return self.__dict__[key]
+    def get_sample_size(self,row,col):
+        return self.get_size(row,col,'ss')
+    def get_effective_size(self,row,col):
+        return self.get_size(row,col,'ne')
 
-def name_generator(row,col):
-    return f'd{row+1}_{col+1}'
-def check_multi_value(flag,value,row,col):
-    if flag:
-        return value[row,col]
-    else:
-        return value
+    def setup_population(self,ne=None,ss=None):
+        if self.ne is None:
+            if ne is None:
+                raise Exception('Effective size is not setup')
+            else:
+                self.set_effective_size(ne)
+        if self.ss is None:
+            if ss is None:
+                raise Exception('Sample size is not setup')
+            else:
+                self.set_sample_size(ss)
+
+        for row in range(self.height):
+            for col in range(self.width):
+                name = CellDemography.name_generator(row,col)
+                self.pop.add_population(name=name,initial_size=self.get_effective_size(row,col))
+                self.samples[name] = self.get_sample_size(row,col)
+        
+    def add_ancestral_pop(self,time_to_merge=200,ancestral_size=None):
+        if ancestral_size == None or ancestral_size < 1:
+            if self.multi_ne:
+                raise ValueError('Ambiguity in the population effective size for ancestral population!')
+            ancestral_size = self.ne
+        self.pop.add_population(name='pan',initial_size=ancestral_size,
+                                description='Ancestral population!')
+        self.pop.add_population_split(time=time_to_merge,ancestral='pan',
+                                      derived=np.arange(self.height*self.width))
+
+
+class DownwardDemography(CellDemography):
+    def __init__(self, height, width, migration_rate) -> None:
+        super().__init__(height, width, migration_rate)
+    def migration_options(self, row, col):
+        return ([row+1,col])
+    def add_migration_path(self, d1, d2):
+        if self.check_migration_path(d1,d2):
+            self.pop.set_migration_rate(source=d2,destination=d1,rate=self.migration_rate)
+            return True
+        return False
+    
+        
+class RecombinationMap():
+    def __init__(self,chr_lengths,recomb_rates) -> None:
+        self.chr_lengths = chr_lengths
+        self.chr_count = len(chr_lengths)
+        self.recomb_rates  = recomb_rates
+        if self.chr_count != len(self.recomb_rates) and len(self.recomb_rates) > 1:
+            raise ValueError('Mismatch in values provided for recombination rate on each chromosome and length of chromosomes')
+        self.border_list = []
+        head = 0
+        for index,chr_len in enumerate(self.chr_lengths):
+            self.border_list.append(head)
+            self.border_list.append(head+chr_len)
+            head += chr_len + 1 
+        self.recomb_list = None
+        if len(self.recomb_rates) == 1 :
+            recomb_list = (self.chr_count*[self.recomb_rates[0],0.5])[:-1]
+        else:
+            recomb_list = np.zeros((2*self.chr_count-1))
+            recomb_list[1::2] = .5
+            recomb_list[::2] = self.recomb_rates
+    
+        self.rate_map = msprime.RateMap(position=self.border_list,rate=recomb_list)
+    def write_to_file(self,ts,output_prefix):
+        for chr_num in range(self.chr_count):
+            start,end = self.border_list[chr_num*2:chr_num*2+2]
+            chrom_ts = ts.keep_intervals([[start, end]], simplify=False).trim()
+            chrom_ts.dump(f'{output_prefix}_chr{chr_num+1}.ts')
+    
+class GridSimulation():
+    def __init__(self) -> None:
+        pass
+    def setup_demography(self,height,width,migration_rate,migration_dir,sample_size,effective_size,ancestral_size,time_to_merge):
+        if migration_dir == 'all':
+            self.demo = CellDemography(height,width,migration_rate)
+        elif migration_dir == 'down':
+            self.demo = DownwardDemography(height,width,migration_rate)
+        else:
+            raise ValueError(f'Migration direction "{migration_dir}" is not supported!')
+        self.demo.set_effective_size(effective_size)
+        self.demo.set_sample_size(sample_size)
+        self.demo.setup_population()
+        self.demo.setup_migration()
+        self.demo.add_ancestral_pop(time_to_merge,ancestral_size)
+    def setup_recombination(self,chr_lengths,recomb_rates):
+        self.recomb = RecombinationMap(chr_lengths,recomb_rates)
+    def setup_model(self,dtwf_duration):
+        self.model = [msprime.DiscreteTimeWrightFisher(duration=dtwf_duration),msprime.StandardCoalescent()]
+    def simulate(self,mu,random_seed=1234):
+        self.random_seed = random_seed
+        self.ts = msprime.sim_ancestry(samples=self.demo.samples,demography=self.demo.pop,model= self.model,random_seed=random_seed,recombination_rate=self.recomb.rate_map)
+        self.mts = msprime.sim_mutations(self.ts,rate=mu,random_seed=random_seed)
+        
+    def write_to_file(self,output_prefix): 
+        self.recomb.write_to_file(self.mts,output_prefix)
+
+
+
 
 
 def main():
@@ -44,7 +182,7 @@ def main():
     args=parser.parse_args()
 
     print(args)
-    demography = msprime.Demography()
+    simulator = GridSimulation()
     width = args.deme_columns
     height = args.deme_rows
     deme_count = width * height
@@ -61,72 +199,11 @@ def main():
     if len(sample_size) != 1 and len(effective_size) != 1 and len(sample_size) != len(effective_size):
         raise ValueError('Discrepancy between the number of initial sample sizes and effect population sizes passed.')
     migration_rate = args.migration_rate
-    multiple_ss = len(sample_size) > 1
-    multiple_ne = len(effective_size) > 1
-    if multiple_ss:
-        sample_size = np.array(sample_size).reshape((height, width))
-    else:
-        sample_size = sample_size[0]
-    if multiple_ne:
-        effective_size = np.array(effective_size).reshape((height,width))
-    else:
-        effective_size = effective_size[0]
-    
-    migration_partial = partial(check_set_symmetric_migration_change, migration_rate=migration_rate, demography=demography, height=height, width=width)
-    samples = {}
-    for row in range(height):
-        for col in range(width):
-            name = name_generator(row,col)
-            temp_ss = check_multi_value(multiple_ss, sample_size, row, col)
-            temp_ne = check_multi_value(multiple_ne, effective_size, row, col)
-            demography.add_population(name=name,initial_size=temp_ne)
-            samples[name] = temp_ss
-    for row in range(height):
-        for col in range(width):
-            if migration_dir == 'all':
-                coordinates = zip([row, row, row-1, row+1],[col-1, col+1, col, col])
-            elif migration_dir == 'down':
-                coordinates = [(row+1,col)]
-            d1 = (row, col)
-            print(list(map(lambda d2: migration_partial(d1,d2),coordinates)))
-    demography.add_population(name='pan',initial_size=ancestral_size,description='Ancestral population!')
-    demography.add_population_split(time=args.time_to_merge,ancestral='pan',derived=np.arange(height*width))
-    model = [msprime.DiscreteTimeWrightFisher(duration=args.dtwf_duration),msprime.StandardCoalescent()]
-
-    chr_lengths = args.chr_length
-    chr_count = len(chr_lengths)
-    recombination_rates = args.rho
-    if chr_count != len(recombination_rates) and len(recombination_rates) > 1:
-        raise ValueError('Mismatch in values provided for recombination rate on each chromosome and length of chromosomes')
-    
-    
-    border_list = []
-    head = 0
-    for index,chr_len in enumerate(chr_lengths):
-        border_list.append(head)
-        border_list.append(head+chr_len)
-        head += chr_len + 1 
-    recomb_list = None
-    if len(recombination_rates) == 1 :
-        recomb_list = (chr_count*[recombination_rates[0],0.5])[:-1]
-    else:
-        recomb_list = np.zeros((2*chr_count-1))
-        recomb_list[1::2] = .5
-        recomb_list[::2] = recombination_rates
-    
-    rate_map = msprime.RateMap(position=border_list,rate=recomb_list)
-    
-
-    mu = args.mu
-    ts = msprime.sim_ancestry(samples=samples,demography=demography,model= model,random_seed=args.random_seed,recombination_rate=rate_map)
-    mts = msprime.sim_mutations(ts,rate=mu,random_seed=args.random_seed)
-    print(border_list)
-    for chr_num in range(chr_count):
-        start,end = border_list[chr_num*2:chr_num*2+2]
-        chrom_ts = mts.keep_intervals([[start, end]], simplify=False).trim()
-        chrom_ts.dump(f'{args.outdir}_chr{chr_num+1}.ts')
-        
-
+    simulator.setup_demography(height,width,migration_rate,migration_dir,sample_size,effective_size,ancestral_size,args.time_to_merge)    
+    simulator.setup_model(args.dtwf_duration)
+    simulator.setup_recombination(args.chr_length,args.rho)
+    simulator.simulate(args.me,args.random_size)
+    simulator.write_to_file(args.outdir)
     
 if __name__ == '__main__':
     main()
