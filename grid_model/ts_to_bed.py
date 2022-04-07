617ac9535ab3b0f9f6bf8d8454db1a4fb141deca
Ruhollah Shemirani <shemirani.r@gmail.com>
2022-04-07 13:25:47 -0400

    improved ts_to_bed.py

--- grid_model/ts_to_bed.py
+++ grid_model/ts_to_bed.py
@@ -5,12 +5,16 @@ def main():
     ts_addr = sys.argv[1]
     border_list_addr = sys.argv[2]
     output_addr = sys.argv[3]
+    maf = 0
     if len(sys.argv) > 4:
         maf = float(sys.argv[4])
+    print('Single tree sequence file to single plink binary file.')
     tio = TreeIO()
-
+    print(f'Loading the tree sequence in memory: {ts_addr}')
     tio.read_from_file(ts_addr,border_list_addr)
+    print('Dividing the TS objects into chromosomes...')
     tio.chr_divider()
+    print(f'Writing variants in this file: {output_addr} - Minimum MAF threshold: {maf}')
     tio.write_bed(output_addr,maf=maf)
 
 if __name__ == '__main__':
