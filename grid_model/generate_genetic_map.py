725198f3eafd52355082dcd6c77340bc71bd9fe5
Ruhollah Shemirani <shemirani.r@gmail.com>
2022-04-11 12:00:35 -0400

    bug fix

--- grid_model/generate_genetic_map.py
+++ grid_model/generate_genetic_map.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys,gzip
 from config import genome_data
-header_line = 'chr position COMBINED_rate(cM/Mb) Genetic_Map(cM)'
+header_line = 'chr position COMBINED_rate(cM/Mb) Genetic_Map(cM)\n'
 def main():
     
     rates = [genome_data[key]['rate'] for key in range(1,23) ]
@@ -10,9 +10,13 @@ def main():
     output_addr = sys.argv[2]
     with open(input_addr,'r') as input_bim:
         with gzip.open(output_addr,'wb') as output_map:
-            output_addr.write(header_line.encode())
+            output_map.write(header_line.encode())
             for line in input_bim:
                 data = line.strip().split()
                 chrom = int(data[0])
                 position = int(data[3])
                 output_map.write(f'{chrom} {position} {combined_rates[chrom-1]} {position*rates[chrom-1]*100}\n'.encode())
+
+
+if __name__ == '__main__':
+    main()
