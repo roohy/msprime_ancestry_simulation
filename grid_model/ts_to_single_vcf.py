0e0729ea71da57bae6e56a21aa1b671da929755d
Ruhollah Shemirani <shemirani.r@gmail.com>
2022-04-18 12:21:34 -0400

    added ts to single vcf for faster performance

---
+++ grid_model/ts_to_single_vcf.py
@@ -0,0 +1,19 @@
+import sys
+from tkinter.tix import Tree
+from recomb_map import TreeIO
+def main():
+    ts_addr = sys.argv[1]
+    border_list_addr = sys.argv[2]
+    output_addr = sys.argv[3]
+    
+    print('Single tree sequence file to single plink binary file.')
+    tio = TreeIO()
+    print(f'Loading the tree sequence in memory: {ts_addr}')
+    tio.read_from_file(ts_addr,border_list_addr)
+    print('Dividing the TS objects into chromosomes...')
+    tio.chr_divider()
+    print(f'Writing variants in this file: {output_addr}')
+    tio.write_single_vcf(output_addr)
+
+if __name__ == '__main__':
+    main()
